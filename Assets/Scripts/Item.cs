13edea6e572693966f08282689284bd7060b794a
Daniel-ECB <danielcorzo@outlook.com>
2019-06-08 15:14:58 -0300

    Commit uno

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,10 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    private List<Tile> _collisionTiles = new List<Tile>();    
+    public int itemNumberOfTiles = 6;
+    public float[] touchingTilesDistance = new float[6];
+    public Tile[] closestTiles = new Tile[6];
+     
+    private List<Tile> _collisionTiles = new List<Tile>();
+    private Collider2D itemCollider;
+
+    private void Awake()
+    {
+        itemCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(!_collisionTiles.Any())
+        {
+            return;
+        }
+
+        // Distancia entre el objeto a poner y cada cuadrado que toca
+        for (int i = 0; i < _collisionTiles.Count; i++)
+        {
+            Collider2D _tileCollider = _collisionTiles[i].GetComponent<Collider2D>();
+            ColliderDistance2D _distance = itemCollider.Distance(_tileCollider);
+
+            // Nos quedamos solo con los más cercanos
+            for (int k = 0; k < touchingTilesDistance.Length; k++)
+            {
+                if (_distance.distance < touchingTilesDistance[i])
+                {
+                    closestTiles[k] = _collisionTiles[i];
+                }
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Collision");
@@ -13,6 +48,16 @@ public class Item : MonoBehaviour
 
         if (!tile.Occupied) {
             tile.ShowGreenFeedback();
+            _collisionTiles.Add(tile);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var tile = collision.GetComponent<Tile>();
+        if (tile == null) return;
+        
+            tile.ResetOriginalColor();
+            _collisionTiles.Remove(tile);
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,12 +5,19 @@ public class Tile : MonoBehaviour {
     public Color tileEnable = Color.green;
     public Color tileOccupied = Color.red;
     public bool Occupied;
+    private Color originalColor;
 
     private void Awake() {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = _spriteRenderer.color;
     }
 
     public void ShowGreenFeedback() {
         _spriteRenderer.color = tileEnable;
+    }
+
+    public void ResetOriginalColor()
+    {
+        _spriteRenderer.color = originalColor;
     }
 }
