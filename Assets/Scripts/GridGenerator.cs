ec1b1c02aa5d56e3092282bab11cb453f710e4c2
Martin Gonzalez <martin.gonzalez@etermax.com>
2019-06-08 17:45:39 -0300

    Modify GridGenerator

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class GridGenerator : MonoBehaviour {
     public float spaceX = 1.2f;
@@ -6,19 +7,49 @@ public class GridGenerator : MonoBehaviour {
     public int tilesX = 10;
     public int tilesY = 10;
     public GameObject tilePrefab;
-    
+    private List<Transform> bla = new List<Transform>();
+    private GameObject innerParent;
+
     public void GenerateGrid() {
+        DeleteChildren();
+        bla = new List<Transform>();
+
         var posX = 0f;
         var posY = 0f;
         for (var i = 0; i < tilesY; i++) {
             for (var j = 0; j < tilesX; j++) {
                 var tile = Instantiate(tilePrefab, transform);
                 tile.transform.localPosition = new Vector3(posX, posY);
+                tile.name = j + "_" + i;
+                bla.Add(tile.transform);
                 posX += spaceX;
             }
 
             posX = 0;
             posY -= spaceY;
-        }            
+        }
+
+        innerParent = new GameObject("Parent");
+        innerParent.transform.SetParent(transform);
+
+        var totalX = 0f;
+        var totalY = 0f;
+        foreach (var tile in bla) {
+            totalX += tile.transform.position.x;
+            totalY += tile.transform.position.y;
+        }
+
+        var centerX = totalX / tilesX * tilesY;
+        var centerY = totalY / tilesX * tilesY;
+
+        innerParent.transform.position = new Vector3(centerX, centerY, transform.position.z);
+        
+        foreach (var tile in bla) {
+            tile.transform.SetParent(innerParent.transform);
+        }
+    }
+
+    private void DeleteChildren() {
+        DestroyImmediate(innerParent);
     }
 }
