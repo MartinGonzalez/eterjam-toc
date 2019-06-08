1a5537ba1575980d6dd158089b5e9e6a00a6bbfe
Daniel-ECB <danielcorzo@outlook.com>
2019-06-08 19:38:26 -0300

    Pasaje entre escenas.

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelData : MonoBehaviour
 {
     public int totalSnapsInLevel;
     public GameObject levelClearMessage;
+    public string sceneToLoad;
 
     private void Start()
     {
         GameManager.instance.InitialSnaps = totalSnapsInLevel;
         GameManager.instance.Snaps = totalSnapsInLevel;
         GameManager.instance.levelDoneMessage = levelClearMessage;
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
