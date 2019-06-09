4fe7475de8fe41807369582412028ddbf3f0afbd
Martin Gonzalez <martin.gonzalez@etermax.com>
2019-06-09 14:56:05 -0300

    The End!

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    public Button button;
+
+    private void Start() {
+        button.onClick.AddListener(() => GoToLevel());
+    }
+
+    private void GoToLevel() {
+        SceneManager.LoadScene("Level01");
+    }
+}
